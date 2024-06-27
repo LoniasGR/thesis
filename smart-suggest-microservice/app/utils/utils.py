bf97df3c11a6c1e996b65587136b97ffa68afaa2
Leonidas Avdelas <avdelasleonidas@gmail.com>
2024-06-27 12:26:38 +0300

    Add smart-suggest-microservice as submodule (Part 1)
    Signed-off-by: Leonidas Avdelas <avdelasleonidas@gmail.com>

--- smart-suggest-microservice/app/utils/utils.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def envOrDefault(variable: str, default: str) -> str:
-     ret: str | None = os.getenv(variable)
-     if ret is None:
-          return default
-     else:
-          return ret
